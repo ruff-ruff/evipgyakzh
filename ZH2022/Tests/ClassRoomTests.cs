f12a7a6d3dbe8e1150a97f24cc206ee6631dbe15
Ruff-Cs <ruffruff@edu.bme.hu>
2025-10-20 20:32:53 +0200

    5. Extra funkcionalitás unit teszt

--- ZH2022/Tests/ClassRoomTests.cs
+++ ZH2022/Tests/ClassRoomTests.cs
@@ -18,5 +18,24 @@ namespace Tests
             cr.Add(Person.GetMe());
             Assert.Equal(3, cr.Count());
         }
+
+        [Fact]
+        public void TestHeightInClass()
+        {
+            var cr = new ClassRoom();
+            var pg = new PersonGrower();
+            pg.N = 2;
+            for (int i = 1; i < 4; i++)
+            {
+                var p = new Person($"{i}. István", 18, 180+i*2);
+                cr.Add(p);
+            }
+            var tallest = cr.GetTallestPerson();
+            var laszlo = new Person("IV. László", 36, 190);
+            Assert.NotEqual(tallest, laszlo);
+            cr.Add(laszlo);
+            tallest = cr.GetTallestPerson();
+            Assert.Equal(tallest, laszlo);
+        }
     }
 }
