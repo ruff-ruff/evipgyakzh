410fc6a8666bfc1e14a894061a0d525f9bf797f6
Ruff-Cs <ruffruff@edu.bme.hu>
2025-10-20 20:42:00 +0200

    6. A konzol alkalmazás készítése

--- ZH2022/ConsoleApplication/Program.cs
+++ ZH2022/ConsoleApplication/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace ConsoleApplication
+﻿using Common;
+
+namespace ConsoleApplication
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var pg = new PersonGrower();
+            pg.N = 5;
+            PrintAndModify(pg);
+        }
+
+        public static void PrintAndModify(IPersonModifier modifier)
+        {
+            var me = Person.GetMe();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(me.GetDiscription());
+                modifier.Modify(me);
+            }
         }
     }
 }
